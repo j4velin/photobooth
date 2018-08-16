2ee5f9da7a09f0319d4eb13dc151c9d37eace57a
j4velin <j4velin@gmail.com>
2018-08-16 21:00:55 +0200

    update trigger.py

--- trigger.py
+++ trigger.py
@@ -1,37 +1,60 @@
-# python script to run on the RaspberryPi with the trigger button
-
-
 import socket
 import sys
 import time
 import RPi.GPIO as GPIO
 import os
+import fcntl
+import struct
+from functools import partial
+from multiprocessing import Pool
 
-IP_PREFIX = '10.5.5.'
 PORT = 5555
 GPIO_TRIGGER_PIN = 4
 TAKE_PHOTO_CMD = "TAKE_PHOTO\n"
+SCANNER_WORKERS = 10
 
-current = 35
-result_ip = None
-print 'searching for Android device...'
-while result_ip is None and current < 255:
-    ip = IP_PREFIX + str(current)
-    sys.stdout.write('testing ' + ip)
-    sys.stdout.flush()
-    sys.stdout.write('\r')
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
 
+def test_port(prefix, suffix):
+    host = prefix + str(suffix)
+    print 'testing ' + str(host)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(2)
-    result = s.connect_ex((ip, PORT))
+    result = s.connect_ex((host, PORT))
+    s.close()
     if result == 0:
-        result_ip = ip
+        return host
     else:
-        current = current + 1
+        return False
 
-if result_ip is None:
-    print "No device found"
-    sys.exit(1)
+def scan_hosts():
+    prefix = get_ip_prefix()
+    p = Pool(SCANNER_WORKERS)
+    test_partial = partial(test_port, prefix)
+    return filter(bool, p.map(test_partial, range(1,255)))
+
+def get_ip_prefix():
+    my_ip = get_ip_address('wlan0') 
+    print 'my ip: ' + str(my_ip)
+    return my_ip[:my_ip.rfind('.')]+'.'
+
+result_ip = None
+
+while result_ip == None:
+    tablet = scan_hosts()
+    print str(tablet)
+
+    if len(tablet) > 0:
+        result_ip = tablet[0]
+    else:
+        print 'no device found'
+        time.sleep(10)
 
 print "Result IP: " + result_ip
 
@@ -45,9 +68,9 @@ try:
     while True:
         input_state = GPIO.input(GPIO_TRIGGER_PIN)
         if input_state == False:
-            print "taking photo..."
+            # print "taking photo..."
             s.send(TAKE_PHOTO_CMD.encode()) 
             time.sleep(5)  
 except KeyboardInterrupt:
     print "keyboard interrupt"
-s.close ()
+s.close()
